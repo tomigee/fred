7fc0be3ba5eb4dfd186f8774d9c4b4675672fac5
tomigee <8400294+tomigee@users.noreply.github.com>
2024-02-25 18:14:55 +0100

    added tags and throttle functionality

--- fred/core.py
+++ fred/core.py
@@ -6,6 +6,9 @@ Core functionality for interacting with the FRED API.
 
 import os
 import requests
+from datetime import datetime, timedelta
+from time import sleep
+import xml.etree.ElementTree as ET
 
 try:
     from itertools import ifilter as filter
@@ -21,6 +24,9 @@ except ImportError:
 class Fred(object):
     """An easy-to-use Python wrapper over the St. Louis FRED API."""
 
+    FIRST_REQUEST_TIMESTAMP = None
+    REQUEST_COUNT = 0
+
     def __init__(self, api_key='', xml_output=False):
         if 'FRED_API_KEY' in os.environ:
             self.api_key = os.environ['FRED_API_KEY']
@@ -35,15 +41,71 @@ class Fred(object):
         path = self.endpoint + '/'.join(args)
         return path
 
-    def get(self, *args, **kwargs):
+    def get(self, *args, throttle, **kwargs):
         """Perform a GET request againt the Fred API endpoint."""
         location = args[0]
         params = self._get_keywords(location, kwargs)
         url = self._create_path(*args)
-        request = requests.get(url, params=params)
-        content = request.content
-        self._request = request
-        return self._output(content)
+
+        # Store time of first request persistently for throttling
+        if Fred.FIRST_REQUEST_TIMESTAMP is None:
+            Fred.FIRST_REQUEST_TIMESTAMP = datetime.now()
+
+        if Fred.REQUEST_COUNT > 1:
+            if throttle:
+                self.__throttle()
+
+        error_flag = True
+        request_retries = 0
+        sleep_secs = 300
+        max_retries = 3
+        while error_flag and (request_retries <= max_retries):
+            request = requests.get(url, params=params)
+            content = request.content
+            self._request = request
+            Fred.REQUEST_COUNT += 1
+            output = self._output(content)
+            error_flag, error_code = self._handle_errors(output)
+
+            if error_flag:
+                request_retries += 1
+                print(f"Request failed with code {error_code}. Retrying...")
+                sleep(sleep_secs)
+
+        return output
+
+    def _handle_errors(self, output):
+        error_flag = False
+        error_code = None
+        if self.xml:
+            root = ET.fromstring(output)
+            if root.tag == 'error':
+                if (root.get('code') == '429') or (root.get('code') == '500'):
+                    error_flag = True
+                    error_code = root.get('code')
+        else:
+            if "error_code" in output:
+                if (output['error_code'] == 429) or (output['error_code'] == 500):
+                    error_flag = True
+                    error_code = output['error_code']
+        return error_flag, error_code
+
+    def __throttle(self):
+        rq_pace_limit = 90/60  # Rate limit (90 requests per minute) in rq/seconds
+        time_delta_secs = (datetime.now() - Fred.FIRST_REQUEST_TIMESTAMP)
+        request_pace = Fred.REQUEST_COUNT / time_delta_secs.total_seconds()
+
+        if request_pace >= rq_pace_limit:
+            print("Throttling...")
+            # Calculate delay
+            delay = timedelta(
+                seconds=((Fred.REQUEST_COUNT + 1)/rq_pace_limit)
+            ) - time_delta_secs
+            delay = delay.total_seconds()  # convert to seconds
+        else:
+            delay = 0
+
+        sleep(delay)
 
     def _get_keywords(self, location, keywords):
         """Format GET request's parameters from keywords."""
@@ -76,51 +138,59 @@ class Fred(object):
             return content
         return json.loads(content)
 
-    def category(self, path=None, **kwargs):
+    def category(self, path=None, throttle=False, **kwargs):
         """
         Get a specific category.
 
         >>> Fred().category(category_id=125)
         """
-        return self.get('category', path, **kwargs)
+        return self.get('category', path, throttle=throttle, **kwargs)
 
-    def release(self, path=None, **kwargs):
+    def release(self, path=None, throttle=False, **kwargs):
         """
         Get a release of economic data.
 
         >>> Fred().release('series', release_id=51)
         """
-        return self.get('release', path, **kwargs)
+        return self.get('release', path, throttle=throttle, **kwargs)
 
-    def releases(self, path=None, **kwargs):
+    def releases(self, path=None, throttle=False, **kwargs):
         """
         Get all releases of economic data.
 
         >>> Fred().releases('dates', limit=10)
         """
-        return self.get('releases', path, **kwargs)
+        return self.get('releases', path, throttle=throttle, **kwargs)
 
-    def series(self, path=None, **kwargs):
+    def series(self, path=None, throttle=False, **kwargs):
         """
         Get economic series of data.
 
         >>> Fred().series('search', search_text="money stock")
         """
-        return self.get('series', path, **kwargs)
+        return self.get('series', path, throttle=throttle, **kwargs)
 
-    def source(self, path=None, **kwargs):
+    def source(self, path=None, throttle=False, **kwargs):
         """
         Get a single source of economic data.
 
         >>> Fred().source(source_id=51)
         """
-        return self.get('source', path, **kwargs)
+        return self.get('source', path, throttle=throttle, **kwargs)
 
-    def sources(self, path=None, **kwargs):
+    def sources(self, path=None, throttle=False, **kwargs):
         """
         Get all of FRED's sources of economic data.
 
         >>> Fred().sources()
         """
-        return self.get('sources', path, **kwargs)
+        return self.get('sources', path, throttle=throttle, **kwargs)
+
+    def tags(self, path=None, throttle=False, **kwargs):
+        """
+        Get all FRED tags of economic data, or search for tags by name.
+
+        >>> Fred().tags()
+        """
+        return self.get('tags', path, throttle=throttle, **kwargs)
 
